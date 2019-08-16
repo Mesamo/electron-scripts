02cd43f938554661b89ad1b76f92aad8c1a0c08e
Mesamo <mesamo@foxmail.com>
2019-08-16 11:25:30 +0800

    feat: webpack打包ts代码

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,31 @@
 const chalk = require('chalk');
 
-console.log(chalk.default.gray('"build" script is not implemented'));
+const copyPublicFolder = require('../lib/compilation/copyPublicFolder');
+const electronPackage = require('../lib/compilation/electronPackage');
+const compileTs = require('../lib/compilation/compileTs');
+
+copyPublicFolder();
+compileTs()
+  .then(({ stats, warnings }) => {
+    if (warnings.length) {
+      console.log(chalk.yellow('Compiled with warnings.\n'));
+      console.log(warnings.join('\n\n'));
+      console.log(
+        '\nSearch for the ' +
+          chalk.underline(chalk.yellow('keywords')) +
+          ' to learn more about each warning.'
+      );
+      console.log(
+        'To ignore, add ' +
+          chalk.cyan('// eslint-disable-next-line') +
+          ' to the line before.\n'
+      );
+    } else {
+      console.log(chalk.green('Compiled successfully.\n'));
+    }
+    return electronPackage();
+  })
+  .catch(error => {
+    console.log(chalk.red(error.message));
+    process.exit(1);
+  });
