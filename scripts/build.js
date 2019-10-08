90ae9ef2ee7c4a7f440e8fa629d080d30aa8ea27
Mesamo <mesamo@foxmail.com>
2019-10-08 14:23:54 +0800

    feat: 添加6.0版本的inno setup

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,18 @@
 const chalk = require('chalk');
 
-const copyFiles = require('../lib/compilation/copyFiles');
-const compileTs = require('../lib/compilation/compileTs');
-const electronPackage = require('../lib/compilation/electronPackage');
+// const copyFiles = require('../lib/compilation/copyFiles');
+// const compileTs = require('../lib/compilation/compileTs');
+// const electronPackage = require('../lib/compilation/electronPackage');
+const installation = require('../lib/compilation/installationPackage');
 
 build();
 
 async function build() {
   try {
-    await copyFiles();
-    await compileTs();
-    await electronPackage();
+    // await copyFiles();
+    // await compileTs();
+    // await electronPackage();
+    await installation();
   } catch (error) {
     console.log(chalk.red(error.message));
     process.exit(1);
