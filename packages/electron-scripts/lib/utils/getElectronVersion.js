caf221e3c77957d41695fbad3ca88e908ead58b1
Mesamo <mesamo@foxmail.com>
2020-08-24 17:26:17 +0800

    fix: 使用参数中electron包名

--- packages/electron-scripts/lib/utils/getElectronVersion.js
+++ packages/electron-scripts/lib/utils/getElectronVersion.js
@@ -1,16 +1,7 @@
 const resolvePackage = require('./resolvePackage');
 
 function getElectronVersion(electronPkgName = 'electron') {
-  return resolvePackage('electron').then((pkg) => pkg.version);
-  // return new Promise((resolve, reject) => {
-  //   nodeResolve(electronPkgName, (error, resolved, pkg) => {
-  //     if (error) {
-  //       reject(error);
-  //     } else {
-  //       resolve(pkg.version);
-  //     }
-  //   });
-  // });
+  return resolvePackage(electronPkgName).then((pkg) => pkg.version);
 };
 
 module.exports = getElectronVersion;
